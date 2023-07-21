af0d7114169238e211afbf7d9937112146a0119e
Naomi S <naomiserkez@gmail.com>
2023-07-21 15:37:48 -0400

    Add comments to blog

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,8 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  resources :articles
+  resources :articles do
+    resources :comments
+  end
  
 end
