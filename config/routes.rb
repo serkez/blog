6b428e59e2ddc7696f002e231275d1a5d7e052ad
Naomi S <naomiserkez@gmail.com>
2023-07-20 23:20:38 -0400

    Change routes to use resources and create CR of CRUD (create new article)

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,6 @@ Rails.application.routes.draw do
   # Defines the root path route ("/")
   root "articles#index"
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-  get "/articles", to: "articles#index"
-  get "/articles/:id", to: "articles#show"
+  resources :articles
  
 end
